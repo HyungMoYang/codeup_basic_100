478e042444b1b152f566751fcdf727fc34448f5c
TipsyBoy <gudah13@naver.com>
2020-03-17 01:03:39 +0900

    Basic output practice

--- codeup_basic_100/test.c
+++ codeup_basic_100/test.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 
+void f_1001(void) {
+	printf("Hello");
+}
+
+void f_1002(void) {
+	printf("Hello World");
+}
+
+void f_1003(void) {
+	printf("Hello\nWorld");
+}
+
+void f_1004(void) {
+	printf("\'Hello\'");
+}
+
+void f_1005(void) {
+	printf("\"Hello World\"");
+}
+
+void f_1006(void) {
+	// %는 형식 지정자(format specifier) 따라서 %를 출력하려면 %%
+	printf("\"!@#$%%^&*()\"");
+}
+
+void f_1007(void) {
+	printf("\"C:\\Download\\hello.cpp\"");
+}
+
+void f_1008(void) {
+	printf("\u250C\u252C\u2510\n");
+	printf("\u251C\u253C\u2524\n");
+	printf("\u2514\u2534\u2518\n");
+}
+
+
+
 int main(void) {
-	printf("시발럼아");
+	//f_1001();
+	//f_1002();
+	//f_1003();
+	//f_1004();
+	//f_1005();
+	//f_1006();
+	//f_1007();
+	//f_1008();
 	return 0;
 }
