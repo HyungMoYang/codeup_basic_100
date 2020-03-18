d0fdbbab501a46c54c3a23a65eaa70ce34f12637
TipsyBoy <gudah13@naver.com>
2020-03-19 01:03:39 +0900

    input output basic 17~22

--- codeup_basic_100/test.c
+++ codeup_basic_100/test.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void f_1001(void) {
@@ -77,15 +78,70 @@ void f_1015(void) {
 	printf("%.2f", a);
 }
 
+void f_1017(void) {
+	int x;
+
+	scanf("%d", &x); // 실
+	printf("%d %d %d", x, x, x);
+}
+
+void f_1018(void) {
+	int h, m;
+
+	scanf("%d:%d", &h, &m); // 실
+	printf("%d:%d", h, m);
+	/*
+	어떤 형식에 맞추어 시간이 입력될 때, 그대로 출력하는 연습을 해보자.
+
+	참고
+	scanf( ) 함수는 지정한 형식(format)에 따라 입력받는 함수이다.
+	따라서, 입력받고자 하는 형식으로 표현해주면 된다.
+
+	예시
+	scanf("%d:%d", &h, &m); //콜론(:) 기호를 기준으로 두 수가 각 변수에 저장된다.
+*/
+}
+
+void f_1019(void) {
+	int y, m, d;
+
+	scanf("%d.%d.%d", &y, &m, &d); // 실
+	printf("%04d.%02d.%02d", y, m, d);
+	/*
+	입력받은 연, 월, 일을 yyyy.mm.dd 형식으로 출력한다.
+	(%02d를 사용하면 2칸을 사용해 출력하는데,
+	한 자리 수인 경우 앞에 0을 붙여 출력한다.)
+	*/
+}
+
+void f_1020(void) {
+	int x, y;
+	scanf("%d-%d", &x, &y);
+	printf("%06d%07d", x, y);
+
+	/*format에 맞게 출력하기 -> 앞자리가 0으로 시작하는 경우 */
+}
+
+void f_1021(void) {
+	char input_data[51] = "";
+	scanf("%s", input_data);
+	printf("%s", input_data);
+}
+
+void f_1022(void) {
+	char input_string[2001];
+	fgets(input_string, 2000, stdin);
+	printf("%s", input_string);
+
+	/*
+	컴파일 에러났음
+	키보드 입력을 위해서 #include <stdio.h> 추가했어야함 
+	*/
+}
 
 int main(void) {
-	//f_1001();
-	//f_1002();
-	//f_1003();
-	//f_1004();
-	//f_1005();
-	//f_1006();
-	//f_1007();
-	//f_1008();
+
+
+
 	return 0;
 }
